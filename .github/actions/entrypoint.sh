cc4ae69e4de6fc9b2b92a324629eb43b84921ae1
u_nation <Endooooooo7@gmail.com>
2019-12-22 05:34:48 +0900

    リファクタ (#7)
    * fix
    
    * add generated code
    
    Co-authored-by: GitHub Web Flow <noreply@github.com>

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-set -e
+set -eu
 
-rm -f ./code-gen/openapi-generator-cli.jar
-cp -f ./modules/openapi-generator-cli/target/openapi-generator-cli.jar ./code-gen/openapi-generator-cli.jar
-
-git config --global user.name u-nation
-git config --global user.email Endooooooo7@gmail.com
-git remote set-url origin https://u-nation:${GITHUB_TOKEN}@github.com/u-nation/openapi-generator.git
+# git setting
+git config --global user.name github-actions
+git config --global user.email noreply@github.com
 
 export GIT_BRANCH="$(git symbolic-ref HEAD --short 2>/dev/null)"
 if [ "$GIT_BRANCH" = "" ] ; then
@@ -14,8 +11,20 @@ if [ "$GIT_BRANCH" = "" ] ; then
   export GIT_BRANCH=${GIT_BRANCH#remotes/origin/};
 fi
 
+git remote set-url origin https://team-lab:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 git checkout $GIT_BRANCH
-git branch -a
-git add .
-git commit -m "add generated code"
-git push origin $GIT_BRANCH
+
+# replace old jar
+rm -f ./code-gen/openapi-generator-cli.jar
+cp -f ./modules/openapi-generator-cli/target/openapi-generator-cli.jar ./code-gen/openapi-generator-cli.jar
+
+# push if diff exists
+set +e
+git diff --exit-code --quiet
+if [ $? -eq 1 ]; then
+  git add .
+  git commit -m "add generated code"
+  git push origin $GIT_BRANCH
+else
+  echo "nothing to commit"
+fi
